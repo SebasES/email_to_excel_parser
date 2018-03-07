1aa0872230e9eddb01c21c7d6115f3aa85b297cd
sebasES <skienit.sk@gmail.com>
2018-03-07 09:43:24 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+ORG_EMAIL   = "@gmail.com"
+FROM_EMAIL  = "some_email" + ORG_EMAIL
+FROM_PWD    = "some_password"
+SMTP_SERVER = "imap.gmail.com"
+SMTP_PORT   = 993
+Subject_Startswith = 'some_subject_string_the_emails_should_start_with'
